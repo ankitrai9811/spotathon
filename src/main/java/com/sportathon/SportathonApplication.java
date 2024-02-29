37c8e87de4584dba5b3f0520b4813c532e5cb51e
Ankit Rai <ankit_r@hcl.com>
2024-02-29 10:50:07 +0530

    first commit

---
+++ src/main/java/com/sportathon/SportathonApplication.java
@@ -0,0 +1,13 @@
+package com.sportathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportathonApplication.class, args);
+	}
+
+}
